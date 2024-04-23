fc92baeee34e40976a867e4f419c34b92a181220
Gabriel Pezo Rojas <156217567+gabrielpezo@users.noreply.github.com>
2024-04-23 21:17:09 +0000

    pokemon api

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Pokemon, Region, Cities, Favourites
 #from models import Person
 
 app = Flask(__name__)
@@ -37,15 +37,248 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def handle_get_all_users():
+    handle_get_all_users = User().query.all()
+    get_all_users = list(map(lambda user:user.serialize(), handle_get_all_users))
+    return jsonify({"data": get_all_users}), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/user/<int:id>', methods=['GET'])
+def handle_get_user(id):
+    get_user = User.query.get_or_404(id)  
+    return jsonify(get_user.serialize()), 200
 
-    return jsonify(response_body), 200
+@app.route('/pokemons', methods=['GET'])
+def handle_get_all_pokemons():
+    handle_get_all_pokemons = Pokemon().query.all()
+    get_all_pokemons = list(map(lambda pokemon:pokemon.serialize(), handle_get_all_pokemons))
+    return jsonify({"data": get_all_pokemons}), 200
+
+@app.route('/pokemons/<int:id>', methods=['GET'])
+def handle_get_pokemon(id):
+    get_pokemon = Pokemon.query.get_or_404(id)  
+    return jsonify(get_pokemon.serialize()), 200
+
+
+@app.route('/regions', methods=['GET'])
+def handle_get_all_regions():
+    handle_get_all_regions = Region().query.all()
+    get_all_regions = list(map(lambda region:region.serialize(), handle_get_all_regions))
+    return jsonify({"data": get_all_regions}), 200
+
+@app.route('/regions/<int:id>', methods=['GET'])
+def handle_get_region(id):
+    get_region = Region.query.get_or_404(id)  
+    return jsonify(get_region.serialize()), 200
+
+@app.route('/cities', methods=['GET'])
+def handle_get_all_cities():
+    handle_get_all_cities = Cities().query.all()
+    get_all_cities = list(map(lambda city:city.serialize(), handle_get_all_cities))
+    return jsonify({"data": get_all_cities}), 200
+
+@app.route('/cities/<int:id>', methods=['GET'])
+def handle_get_city(id):
+    get_city = Cities.query.get_or_404(id)  
+    return jsonify(get_city.serialize()), 200
+
+@app.route('/favourites', methods=['GET'])
+def handle_get_all_favourites():
+    handle_get_all_favourites = Favourites().query.all()
+    get_all_favourites = list(map(lambda city:city.serialize(), handle_get_all_favourites))
+    return jsonify({"data": get_all_favourites}), 200
+
+@app.route('/favourites/<int:id>', methods=['GET'])
+def handle_get_favourites(id):
+    get_favourite = Favourites.query.get_or_404(id)  
+    return jsonify(get_favourite.serialize()), 200
+
+
+
+@app.route('/create', methods=['POST'])
+def handle_user():
+    data = request.get_json()
+    create_region = User()
+    create_region.name = data["name"]
+    create_region.email = data["email"]
+    create_region.password = data["password"]
+    create_region.is_active = data["active"]
+
+    db.session.add(create_region)
+    db.session.commit()
+
+    return jsonify({"data": data}), 200
+
+@app.route('/pokemon', methods=['POST'])
+def handle_pokemon():
+    data = request.get_json()
+    create_pokemon = Pokemon()
+    create_pokemon.name = data["name"]
+    create_pokemon.type = data["type"]
+    create_pokemon.second_type = data["second_type"]
+    create_pokemon.height = data["height"]
+    create_pokemon.weight = data["weight"]
+    create_pokemon.primary_ability = data["primary_ability"]
+    create_pokemon.secondary_ability = data["secondary_ability"]
+    create_pokemon.hidden_ability = data["hidden_ability"]
+
+    db.session.add(create_pokemon)
+    db.session.commit()
+
+    return jsonify({"data": data}), 200
+
+@app.route('/create_regions', methods=['POST'])
+def handle_region():
+    data = request.get_json()
+    create_region = Region()
+    create_region.name = data["name"]
+    create_region.number_of_cities = data["number_of_cities"]
+    create_region.professor = data["professor"]
+    create_region.initials_id = data["initials_id"]
+
+    db.session.add(create_region)
+    db.session.commit()
+
+    return jsonify({"data": data}), 200
+
+@app.route('/create_city', methods=['POST'])
+def handle_city():
+    data = request.get_json()
+    create_city = Cities()
+    create_city.name = data["name"]
+    create_city.gym_leader = data["gym_leader"]
+    create_city.city_region_id = data ["city_region_id"]
+
+    db.session.add(create_city)
+    db.session.commit()
+
+    return jsonify({"data": data}), 200
+
+@app.route('/create_favourite', methods=['POST'])
+def handle_favourites():
+    data = request.get_json()
+    create_favourites = Favourites()
+    create_favourites.person_id = data["person_id"]
+    create_favourites.pokemon_id = data["pokemon_id"]
+
+    db.session.add(create_favourites)
+    db.session.commit()
+
+    return jsonify({"data": data}), 200
+
+
+
+@app.route('/user/<int:id>', methods=['PUT'])
+def handle_update_user(id):
+    update_user = User.query.get_or_404(id)
+    data = request.get_json()
+    if "name" in data:
+        update_user.name = data["name"]
+    if "email" in data:
+        update_user.email = data["email"]
+    if "password" in data:
+        update_user.password = data["password"]
+
+    db.session.commit()
+    return jsonify(update_user.serialize()), 200
+
+@app.route('/pokemons/<int:id>', methods=['PUT'])
+def handle_update_pokemon(id):
+    update_pokemon = Pokemon.query.get_or_404(id)
+    data = request.get_json()
+    if "name" in data:
+        update_pokemon.name = data["name"]
+    if "type" in data:
+        update_pokemon.type = data["type"]
+    if "second_type" in data:
+        update_pokemon.second_type = data["second_type"]
+    if "height" in data:
+        update_pokemon.height = data["height"]
+    if "weight" in data:
+        update_pokemon.weight = data["weight"]
+    if "primary_ability" in data:
+        update_pokemon.primary_ability = data["primary_ability"]
+    if "secondary_ability" in data:
+        update_pokemon.secondary_ability = data["secondary_ability"]
+    if "hidden_ability" in data:
+        update_pokemon.hidden_ability = data["hidden_ability"]
+
+    db.session.commit()
+    return jsonify(update_pokemon.serialize()), 200
+
+@app.route('/regions/<int:id>', methods=['PUT'])
+def handle_update_region(id):
+    update_region = Region.query.get_or_404(id)
+    data = request.get_json()
+    if "name" in data:
+        update_region.name = data["name"]
+    if "number_of_cities" in data:
+        update_region.number_of_cities = data["number_of_cities"]
+    if "professor" in data:
+        update_region.professor = data["professor"]
+
+    db.session.commit()
+    return jsonify(update_region.serialize()), 200
+
+@app.route('/cities/<int:id>', methods=['PUT'])
+def handle_update_city(id):
+    update_city = Cities.query.get_or_404(id)
+    data = request.get_json()
+    if "name" in data:
+        update_city.name = data["name"]
+    if "gym_leader" in data:
+        update_city.gym_leader = data["gym_leader"]
+
+    db.session.commit()
+    return jsonify(update_city.serialize()), 200
+
+@app.route('/favourites/<int:id>', methods=['PUT'])
+def handle_update_favourites(id):
+    update_favourites = Favourites.query.get_or_404(id)
+    data = request.get_json()
+    if "pokemon_id" in data:
+        update_favourites.pokemon_id = data["pokemon_id"]
+
+    db.session.commit()
+    return jsonify(update_favourites.serialize()), 200
+
+
+
+@app.route('/user/<int:id>', methods=['DELETE'])
+def delete_user(id):
+    get_user = User.query.get_or_404(id)
+    db.session.delete(get_user)
+    db.session.commit()
+    return jsonify({"msg": "User was deleted"}), 200
+
+@app.route('/pokemons/<int:id>', methods=['DELETE'])
+def delete_pokemon(id):
+    get_pokemon = Pokemon.query.get_or_404(id)
+    db.session.delete(get_pokemon)
+    db.session.commit()
+    return jsonify({"msg": "Pokemon was deleted"}), 200
+
+@app.route('/regions/<int:id>', methods=['DELETE'])
+def delete_region(id):
+    get_region = Region.query.get_or_404(id)
+    db.session.delete(get_region)
+    db.session.commit()
+    return jsonify({"msg": "Region was deleted"}), 200
+
+@app.route('/cities/<int:id>', methods=['DELETE'])
+def delete_city(id):
+    get_city = Cities.query.get_or_404(id)
+    db.session.delete(get_city)
+    db.session.commit()
+    return jsonify({"msg": "City was deleted"}), 200
+
+@app.route('/favourites/<int:id>', methods=['DELETE'])
+def delete_favourites(id):
+    get_favourites = Favourites.query.get_or_404(id)
+    db.session.delete(get_favourites)
+    db.session.commit()
+    return jsonify({"msg": "Favourite elements were deleted"}), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
+    app.run(host='0.0.0.0', port=PORT, debug=True)
--- src/models.py
+++ src/models.py
@@ -3,17 +3,94 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(10), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    #def __repr__(self):
+        #return '<User %r>' % self.username
 
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
             "email": self.email,
-            # do not serialize the password, its a security breach
+        }
+
+class Pokemon(db.Model):
+    __tablename__ = "pokemon"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    type = db.Column(db.String(100), unique=False, nullable=False)
+    second_type = db.Column(db.String(100), unique=False, nullable=True) 
+    height = db.Column(db.String(100), unique=False, nullable=False)
+    weight = db.Column(db.String(100), unique=False, nullable=False)
+    primary_ability = db.Column(db.String(100), unique=False, nullable=False)
+    secondary_ability = db.Column(db.String(100), unique=False, nullable=True)
+    hidden_ability = db.Column(db.String(100), unique=False, nullable=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name, 
+            "type": self.type,
+            "second_type": self.second_type, 
+            "height": self.height,
+            "weight": self.weight,
+            "primary_ability": self.primary_ability,
+            "secondary_ability": self.secondary_ability,
+            "hidden_ability": self.hidden_ability,
+        }
+
+class Region(db.Model):
+    __tablename__ = "region"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=False, nullable=False)
+    number_of_cities = db.Column(db.String(10), unique=False, nullable=False)
+    professor = db.Column(db.String(100), unique=False, nullable=False)
+    initials_id = db.Column(db.Integer, db.ForeignKey('pokemon.id'))
+    initials = db.relationship(Pokemon, uselist=False)
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name, 
+            "number_of_cities": self.number_of_cities,
+            "professor": self.professor,
+            "initials_id": self.initials.name,
+        }
+  
+class Cities(db.Model):
+    __tablename__ = "cities"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    gym_leader = db.Column(db.String(100), unique=True, nullable=False)
+    city_region_id = db.Column(db.Integer, db.ForeignKey('region.id'))
+    city_region = db.relationship(Region, uselist=False)
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gym_leader": self.gym_leader, 
+            "city_region_id": self.city_region.name
+        }
+
+class Favourites(db.Model):
+    __tablename__ = "favourites"
+    id = db.Column(db.Integer, primary_key=True)
+    person_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    person = db.relationship(User, uselist=False)
+    pokemon_id = db.Column(db.Integer, db.ForeignKey('pokemon.id'))
+    favourite_pokemon = db.relationship(Pokemon, uselist=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "person_id": self.person.name, 
+            "pokemon_id": self.favourite_pokemon.name,
         }
